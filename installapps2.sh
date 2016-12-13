315b27c886929d98b8a656a6b56beeea1a215ad0
obrevenge <ajjames31@gmail.com>
2016-12-13 16:37:32 -0600

    fixed chroot issue

--- installapps2.sh
+++ installapps2.sh
@@ -11,33 +11,12 @@ utils= ` cat utils.txt `
 
 
 # Installing Selecting Applications
-(
-sleep 2
-echo "25"
-echo "# Installing Internet Applications..."
+
 sudo pacstrap /mnt $int
 
-echo "40"
-echo "# Installing Media Applications..."
 sudo pacstrap /mnt $med
 
-echo "65"
-echo "# Installing Office Applications..."
 sudo pacstrap /mnt $off
 
-echo "75"
-echo "# Installing Utilities..."
 sudo pacstrap /mnt $utils
 
-echo "85"
-echo "# Cleaning up Installation..."; sleep 2
-
-echo "100"
-echo "# Installation Finished!"
-
-
-) | zenity --progress \
-	-- title="OBRevenge Software Tool" \ 
-	--text="Preparring for Installation" \
-	--percentage=10 \
-	--pulsate
