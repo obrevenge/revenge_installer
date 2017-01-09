7d04f04f0aa8d0e0303c639cdf4e61323071785b
obrevenge <ajjames31@gmail.com>
2017-01-09 09:31:26 -0600

    added vbox recognition

--- internetlist2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# OBRevenge Internet list tool
-
-# This is a tool to make a list of internet applications to install 
-
-    zenity --list --text "Select the Internet Applications that You Would Like to Install" --checklist --column "Select" --column "Applications" FALSE "firefox " FALSE "chromium " FALSE "midori " FALSE "qupzilla " FALSE "netsurf " FALSE "filezilla " FALSE "opera " FALSE "evolution " FALSE "geary " FALSE "thunderbird " FALSE "transmission-gtk " FALSE "qbittorrent " FALSE "hexchat " --height 400 > int2.txt
-    sed -i -e 's/[|]//g' int2.txt
-
--- medialist2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# OBRevenge Media list tool
-
-# This is a tool to make a list of media applications to install 
-
-
-    zenity --list --text "Select the Media Applications that You Would Like to Install" --checklist --column "Select" --column "Applications" FALSE "gimp " FALSE "vlc " FALSE "totem " FALSE "parole " FALSE "audacious " FALSE "clementine " FALSE "gthumb " FALSE "shotwell " FALSE "ristretto " FALSE "gpicview " FALSE "brasero " FALSE "audacity " FALSE "simplescreenrecorder " FALSE "xfburn " FALSE "kdenlive " --height 400 > med2.txt
-    sed -i -e 's/[|]//g' med2.txt
